af7efb769c02eb35276207684066f1c8d63a25fe
Jamolbek Pulatov <neugadaypj@gmail.com>
2026-05-06 10:17:04 +0500

    .

--- server.js
+++ server.js
@@ -85,8 +85,8 @@ const Feedback = require('./models/Feedback');
 
 // --- 3. MIDDLEWARE & SETTINGS ---
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true, limit: '50mb' })); 
+app.use(express.json({ limit: '50mb' })); 
 app.use(express.static('public')); 
 app.use(cookieParser()); 
 
